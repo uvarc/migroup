7af3861bc34bf6c53ceee929820ccd301c6b13a3
cagancayco <christina.gancayco@gmail.com>
2022-02-18 12:46:39 -0500

    first commit

---
+++ Test.R
@@ -0,0 +1,10 @@
+library(shiny)
+library(DT)
+
+setwd("/project/MIGROUP/SDMS/Bi2Te2p7Se0p3/")
+data <- read.csv("Data_train_it3.csv", header=T)
+#a <- seq(1:13)
+
+#plotting <- cbind(a, data$Seebeck)
+#colnames(plotting) <- c("Number", "Seebeck")
+
---
+++ app.R
@@ -0,0 +1,29 @@
+library(shiny)
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+df <- read_csv('Data_train_it3.csv')
+
+
+ui <- fluidPage(
+  fluidRow(
+    column(4, selectInput('x', 'X', choices = names(df)),
+           selectInput('y', 'Y', choices = names(df))),
+    column(8, plotOutput('myPlot'))
+  ),
+  tableOutput('myTable')
+)
+
+server <- function(input, output, session) {
+  
+  output$myPlot <- renderPlot({
+    ggplot(df, aes(x = !!as.name(input$x), y = !!as.name(input$y))) + geom_point()
+  })
+  
+  output$myTable <- renderTable({
+    df
+  })
+}
+
+shinyApp(ui, server)
